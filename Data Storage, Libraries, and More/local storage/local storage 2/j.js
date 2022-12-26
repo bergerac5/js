810eed07f42ec620d8498fdd18f9af9f50053277
bergerac5 <birakarama5@gmail>
2022-12-26 19:16:05 +0200

    js

---
+++ Data Storage, Libraries, and More/local storage/local storage 2/j.js
@@ -0,0 +1,61 @@
+let todos = []
+
+
+
+const filter = {
+    searchText:'',
+    completed: false
+}
+
+const todosJSON = localStorage.getItem('notes')
+
+if (notes!==null) {
+    notes=JSON.parse(todosJSON)
+}
+
+const renderTodos = function (todos,filter) {
+    let filteredTodos = todos.filter(function (todo) {
+        const searchTextMatch = todo.text.toLowerCase().includes(filter.searchText.toLowerCase())
+        const completed = !filter.completed || !todo.completed
+        return searchTextMatch && completed
+    })
+
+
+    const incompletedTodos = filteredTodos.filter(function(todo) {
+        return !todo.completed
+    })
+
+    document.querySelector('#sea').innerHTML = ''
+    
+    const summary =  document.createElement('h2')
+    summary.textContent = `You have ${incompletedTodos.length} todos left`
+    
+    document.querySelector('#sea').appendChild(summary)
+    
+    filteredTodos.forEach(function(todo){
+       const p =document.createElement('p')
+       p.textContent = todo.text
+       document.querySelector('#sea').appendChild(p) 
+    })
+}
+renderTodos(todos, filter)
+
+document.querySelector('#input').addEventListener('input', function (e) {
+    filter.searchText = e.target.value
+    renderTodos(todos, filter)
+})
+
+document.querySelector('#form').addEventListener('submit', function (e) {
+    e.preventDefault()
+    todos.push({
+        text: e.target.elements.add.value,
+        completed: false
+    })
+    renderTodos(todos, filter)
+    e.target.elements.add.value = ''
+})
+
+document.querySelector('#completed').addEventListener('change', function (e){
+    filter.completed=e.target.checked
+    renderTodos(todos, filter)
+})
